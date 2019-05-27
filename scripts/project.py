e6911db14cc6f4b1d395cecf5fa4661999fdd3f0
xiaopilang <2422137977@qq.com>
2019-05-27 10:36:59 +0800

    新建项目

---
+++ scripts/project.py
@@ -0,0 +1,8 @@
+import  os
+import  allure
+class Test:
+    def test_001(self):
+        print("测试开始")
+
+    with open(os.getcwd() + os.sep + "scripts/4.PNG", "rb")as f:
+        allure.attach("截图", f.read(), allure.attach_type.PNG)
